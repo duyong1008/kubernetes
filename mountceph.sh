d5a473490efd2c3ab66d87ce4f1f3e79408b451d
duyong1008 <67739649@qq.com>
2018-11-09 10:25:34 +0800

    add helm charts

--- mountceph.sh
+++ mountceph.sh
@@ -1,1 +1,1 @@
-mount -t ceph node01,node02,node03:6789:/ /data/cephfs -o name=admin,secret=AQDscNFbtDoAHxAAXS7BYjxxw/WE3EwuUldLtw==
+mount -t ceph kmaster-92,kmaster-97,kmaster-106:6789:/ /data/cephfs -o name=admin,secret=AQADYeFbM/WIIxAAYI3G/NGpyif6zQgUX3wXbw==
